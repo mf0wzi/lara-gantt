284b6350b770e2c6404bfe657836282f2d967c2d
Mohammed Fawzi <30725615+noonenew@users.noreply.github.com>
2020-07-17 20:05:31 +0300

    Update gantt-template.blade.php

--- src/resources/views/gantt-template.blade.php
+++ src/resources/views/gantt-template.blade.php
@@ -1,35 +1,7 @@
-<div id="{!! $element !!}" style="width: auto; height: {!! $size['height'] !!}px; position: relative; overflow: hidden; background-color: transparent;">
+<div id="{!! $element !!}_gantt" style="width: auto; height: {!! $size['height'] !!}px; position: relative;"></div>
 
-</div>
 <script type="application/javascript">
-    document.addEventListener("DOMContentLoaded", function(event) {
-        (function() {
-            "use strict";
-            let map = L.map('{!! $element !!}', {scrollWheelZoom: false,maxZoom: 18}).setView([15.387664,47.190976],6.2);
-            let basemap = L.tileLayer('{!! $tile["maplink"] !!}', {
-                attribution: '{!! $tile["attribution"] !!}'
-            }).addTo(map);
-
-            //option here
-	    
-            let datasets = JSON.parse({!! $datasets !!});
-
-            //dataset here
-	    
-            datasets.forEach(function (datas) {
-            let tooltips = "<br/>" +
-                "<br/>";
-
-            let popup = "<h4>Spot-light</h4><br/>" +
-                "<lable><b>Company Name :</b> "+datas.latitude+".</lable><br/>" +
-                "<lable><b>Beneficiary Name in Arabic :</b> "+datas.longitude+".</lable><br/>" +
-                "<br/>";
-
-                var m = L.marker([datas.latitude, datas.longitude]).bindPopup(popup).bindTooltip(tooltips,
-                {className: "leaflet-label-marker", offset: [0, 0] }).addTo(map);
-
-            });
-	    
-        })();
-    });
+    let data = {!! $data !!};
+    gantt.init("{!! $element !!}_gantt");
+    gantt.parse(data);
 </script>
